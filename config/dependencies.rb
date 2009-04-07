a6b467e5300a7b93e33390a1ddb73a4dc610fd48
Ernest Ellingson <ellingson@min.powrnav.com>
2009-04-07 10:26:05 -0400

    fixed layout to allow text centering only when no current_user.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,7 +1,7 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
 merb_gems_version = "1.0.11"
 dm_gems_version   = "0.9.11"
-
+do_gems_version   = "0.9.11"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -16,10 +16,15 @@ dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
  
+dependency "data_objects", do_gems_version
+dependency "do_sqlite3", do_gems_version # If using another database, replace this
 dependency "dm-core", dm_gems_version         
 dependency "dm-aggregates", dm_gems_version   
 dependency "dm-migrations", dm_gems_version   
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
+dependency "dm-serializer", dm_gems_version   
+
+dependency "merb_datamapper", merb_gems_version
 dependency "twitter","0.4.2" 
